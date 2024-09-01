4f023a2ed70a5687e7793dfcd9eb7c65962379b0
elvis <slv_evs@yahoo.com.br>
2024-09-01 10:58:24 -0300

    created a server file and turned app into a module

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Olá mundo');
 });
 
-// liste port 3000
-app.listen(port, () => {
-    console.log(`Servidor rodando no endereço http://localhost:${port}`)
-});
+export default app;
