1b1c5e75242c9c998db647bba577ce94e5a39fd5
elvis <slv_evs@yahoo.com.br>
2024-09-03 00:14:36 -0300

    created function to edit a selection

--- src/app.js
+++ src/app.js
@@ -24,21 +24,28 @@ app.get('/', (req, res) => {
 
 app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes);
-})
+});
 
 app.get('/selecoes/:id', (req, res) => {
     return res.json(buscarSelecaoPorId(req.params.id));
 });
 
 app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body)
+    selecoes.push(req.body);
     res.status(201).send({"selecoes": selecoes, "msg": "Seleção cadastrada com sucesso!"});
-})
+});
 
 app.delete('/selecoes/:id', (req, res) => {
     let index = buscarIndexSelecao(req.params.id);
     selecoes.splice(index, 1);
     res.send(`Seleção com id ${index} excluída com sucesso!`);
+});
+
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id);
+    selecoes[index].selecao = req.body.selecao;
+    selecoes[index].grupo = req.body.grupo;
+    res.json(selecoes);
 });
 
 export default app;
