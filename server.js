ab913637b1cc35f63056fdf4025a76cfbc10c595
elvis <slv_evs@yahoo.com.br>
2024-09-05 20:35:50 -0300

    removed the connection from the server file

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
 import app from "./src/app.js"
-import connection from "./infra/connection.js"
 
 const PORT = process.env.PORT || 3000
 
-connection.connect((error) => {
-    if(error) {
-        console.error(error);
-    } else {
-        console.log("conectado");
-
-        // liste port 3000
-        app.listen(PORT, () => {
-            console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
-        })
-    }
+app.listen(PORT, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
 })
 
