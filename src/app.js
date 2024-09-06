94085bf9f4862a1ef43c0bd6ff0e1d4b6ee64c6b
elvis <slv_evs@yahoo.com.br>
2024-09-06 20:25:36 -0300

    removed blank lines and adjusted some status code

--- src/app.js
+++ src/app.js
@@ -3,17 +3,10 @@ import connection from "../infra/connection.js";
 const app = express()
 app.use(express.json())
 
-function buscarSelecaoPorId(id) {
-    return selecoes.filter(selecao => selecao.id == id);
-}
-
-function buscarIndexSelecao(id) {
-    return selecoes.findIndex(selecao => selecao.id == id);
-}
-
 // rotas
 app.get('/selecoes', (req, res) => {
     const sql = "SELECT * FROM selecoes";
+
     connection.query(sql, (error, result) => {
         if(error) {
             console.log('error');
@@ -22,7 +15,6 @@ app.get('/selecoes', (req, res) => {
             res.status(200).json(result);
         }
     })
-
 });
 
 app.get('/selecoes/:id', (req, res) => {
@@ -45,10 +37,9 @@ app.post('/selecoes', (req, res) => {
     const sql = "INSERT INTO selecoes SET ?";
 
     connection.query(sql, selecao, (error, result) => {
-
         if(error) {
             console.log('error');
-            res.status(400).json({msg: "query error", error: error});
+            res.status(404).json({msg: "query error", error: error});
         } else {
             res.status(201).json(result);
         }
@@ -77,7 +68,7 @@ app.put('/selecoes/:id', (req, res) => {
     connection.query(sql, [selecao, id], (error, result) => {
         if(error) {
             console.log('error');
-            res.status(400).json({msg: "query error", error: error});
+            res.status(404).json({msg: "query error", error: error});
         } else {
             res.status(200).json(result);
         }
