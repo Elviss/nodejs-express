bb9923188de4d8aeeeaa92b9a2ca3159a3f89e12
elvis <slv_evs@yahoo.com.br>
2024-09-05 21:40:42 -0300

    refactored the functions to treat database data

--- src/app.js
+++ src/app.js
@@ -1,14 +1,7 @@
 import express from 'express'
+import connection from "../infra/connection.js";
 const app = express()
 app.use(express.json())
-
-// mock
-const selecoes = [
-    {id: 1, selecao: "Brasil", grupo: "G"},
-    {id: 2, selecao: "Suiça", grupo: "G"},
-    {id: 3, selecao: "Servia", grupo: "G"},
-    {id: 4, selecao: "Camarões", grupo: "G"},
-]
 
 function buscarSelecaoPorId(id) {
     return selecoes.filter(selecao => selecao.id == id);
@@ -18,34 +11,80 @@ function buscarIndexSelecao(id) {
     return selecoes.findIndex(selecao => selecao.id == id);
 }
 
-app.get('/', (req, res) => {
-    res.send('Olá mundo');
-});
-
+// rotas
 app.get('/selecoes', (req, res) => {
-    res.status(200).send(selecoes);
+    const sql = "SELECT * FROM selecoes";
+    connection.query(sql, (error, result) => {
+        if(error) {
+            console.log('error');
+            res.status(404).json({msg: "query error", error: error});
+        } else {
+            res.status(200).json(result);
+        }
+    })
+
 });
 
 app.get('/selecoes/:id', (req, res) => {
-    return res.json(buscarSelecaoPorId(req.params.id));
+    const id = req.params.id;
+    const sql = "SELECT * FROM selecoes WHERE id = ?";
+
+    connection.query(sql, id, (error, result) => {
+        const linha = result[0];
+        if(error) {
+            console.log('error');
+            res.status(404).json({msg: "query error", error: error});
+        } else {
+            res.status(200).json(linha);
+        }
+    })
 });
 
 app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body);
-    res.status(201).send({"selecoes": selecoes, "msg": "Seleção cadastrada com sucesso!"});
+
+    const selecao = req.body;
+    const sql = "INSERT INTO selecoes SET ?";
+
+    connection.query(sql, selecao, (error, result) => {
+
+        if(error) {
+            console.log('error');
+            res.status(400).json({msg: "query error", error: error});
+        } else {
+            res.status(201).json(result);
+        }
+    })
 });
 
 app.delete('/selecoes/:id', (req, res) => {
-    let index = buscarIndexSelecao(req.params.id);
-    selecoes.splice(index, 1);
-    res.send(`Seleção com id ${index} excluída com sucesso!`);
+
+    const id = req.params.id;
+    const sql = "DELETE FROM selecoes WHERE id = ?";
+
+    connection.query(sql, id, (error, result) => {
+        if(error) {
+            console.log('error');
+            res.status(404).json({msg: "query error", error: error});
+        } else {
+            res.status(200).json(result);
+        }
+    })
 });
 
 app.put('/selecoes/:id', (req, res) => {
-    let index = buscarIndexSelecao(req.params.id);
-    selecoes[index].selecao = req.body.selecao;
-    selecoes[index].grupo = req.body.grupo;
-    res.json(selecoes);
+
+    const id = req.params.id;
+    const selecao = req.body;
+    const sql = "UPDATE selecoes SET ? WHERE id = ?";
+
+    connection.query(sql, [selecao, id], (error, result) => {
+        if(error) {
+            console.log('error');
+            res.status(400).json({msg: "query error", error: error});
+        } else {
+            res.status(200).json(result);
+        }
+    })
 });
 
 export default app;
