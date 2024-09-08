9cf4889766139ba672fd5d5d30457469b897c6dc
elvis <slv_evs@yahoo.com.br>
2024-09-08 17:46:10 -0300

    new method to execute queries in the database

--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -20,4 +20,22 @@ connection.connect((error) => {
     }
 });
 
+/**
+ * Execute a sql with or without values
+ * @param {string} sql sql instruction to be executed
+ * @param {string=id | [selecao, id]} values values to passed into the sql
+ * @param {string} rejectMessage message to be showed
+ * @returns {Promise<unknown>}
+ */
+export const query = (sql, values = '', rejectMessage) => {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, values, (error, result) => {
+            if (error) return reject(rejectMessage)
+
+            const rows = JSON.parse(JSON.stringify(result))
+            return resolve(rows)
+        })
+    })
+}
+
 export default connection
