b24098316e74b8047380a0866c6fd1d8f63756d9
elvis <slv_evs@yahoo.com.br>
2024-09-08 17:46:56 -0300

    refactory: used the query meethod created to uxecute queries in the database

--- src/app/repositories/SelecaoRepository.js
+++ src/app/repositories/SelecaoRepository.js
@@ -1,69 +1,33 @@
-import connection from "../database/connection.js";
+import { query } from "../database/connection.js";
 
 class SelecaoRepository {
 
     create(selecao) {
         const sql = "INSERT INTO selecoes SET ?";
-        return new Promise((resolve, reject) => {
-            connection.query(sql, selecao, (error, result) => {
-                if (error) return reject('Não foi possível cadastrar')
-
-                const rows = JSON.parse(JSON.stringify(result))
-                return resolve(rows)
-            })
-        })
+        return query(sql, selecao, 'Não foi possível cadastrar!')
     }
 
     findAll() {
         const sql = "SELECT * FROM selecoes";
-        return new Promise((resolve, reject) => {
-            connection.query(sql, (error, result) => {
-                if(error) return reject('Não foi possível localizar')
-
-                const rows = JSON.parse(JSON.stringify(result))
-                return resolve(rows)
-            })
-        })
+        return query(sql, 'Não foi possível localizar!')
     }
 
     findById(id) {
-
         const sql = "SELECT * FROM selecoes WHERE id = ?";
 
-        return new Promise((resolve, reject) => {
-            connection.query(sql, id, (error, result) => {
-                if(error) return reject('Não foi possível localizar')
-
-                const rows = JSON.parse(JSON.stringify(result))
-                return resolve(rows)
-            })
-        })
+        return query(sql, id, 'Não foi possível localizar!')
     }
 
     update(selecao, id) {
         const sql = "UPDATE selecoes SET ? WHERE id = ?";
 
-        return new Promise((resolve, reject) => {
-            connection.query(sql, [selecao, id], (error, result) => {
-                if(error) return reject('Não foi possível atualizar')
-
-                const rows = JSON.parse(JSON.stringify(result))
-                return resolve(rows)
-            })
-        })
+        return query(sql, [selecao, id], 'Não foi possível atualizar!')
     }
 
     delete(id) {
         const sql = "DELETE FROM selecoes WHERE id = ?";
 
-        return new Promise((resolve, reject) => {
-            connection.query(sql, id, (error, result) => {
-                if (error) return reject('Não foi possível atualizar')
-
-                const rows = JSON.parse(JSON.stringify(result))
-                return resolve(rows)
-            })
-        })
+        return query(sql, id, 'Não foi possível deletar!')
     }
 }
 
