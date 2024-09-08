4728f48e4125120c1e9c4e0554e61ae2e294941f
elvis <slv_evs@yahoo.com.br>
2024-09-08 17:56:33 -0300

    created a routes files

--- src/app.js
+++ src/app.js
@@ -1,18 +1,9 @@
 import express from 'express'
-import SelecaoController from "./app/controller/SelecaoController.js";
+import routes from "./routes.js";
 
 const app = express()
+
+app.use(routes)
 app.use(express.json())
-
-// rotas
-app.get('/selecoes', SelecaoController.index);
-
-app.get('/selecoes/:id', SelecaoController.show);
-
-app.post('/selecoes', SelecaoController.store);
-
-app.delete('/selecoes/:id', SelecaoController.delete);
-
-app.put('/selecoes/:id', SelecaoController.update);
 
 export default app;
